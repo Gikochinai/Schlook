4696a27d72cb541920a1b1bff391925b281ae24e
Gikochinai <98325710+Gikochinai@users.noreply.github.com>
2022-05-06 13:55:17 +0800

    Update website

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ var y = document.getElementById('computer');
 var z = document.getElementById('room');
 var u = document.getElementById('books');
 var p = document.getElementById('kekchen');
-var i = document.getElementById('container');
+// var i = document.getElementById('container');
   x.style.display = "none";
   y.style.display = "none";
   z.style.display = "none";
   u.style.display = "none";
   p.style.display = "none";
-  i.style.display = "none";
+  // i.style.display = "none";
 $("#show").click(function(){
   if (x.style.display === "none") {
     x.style.display = "block";
@@ -56,11 +56,11 @@ $("#show4").click(function(){
 
 });
 
-$("#btn-drop-down").click(function(){
-  if (i.style.display === "none") {
-    i.style.display = "block";
-  } else {
-    i.style.display = "none";
-  }
-
-});
+// $("#btn-drop-down").click(function(){
+//   if (i.style.display === "none") {
+//     i.style.display = "block";
+//   } else {
+//     i.style.display = "none";
+//   }
+//
+// });
